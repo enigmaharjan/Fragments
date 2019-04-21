9ac7b14a764a50a120fa848204e2484008c3cba8
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 10:48:38 +0545

    Fragment added to mainActivity

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.fragments;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+
+import fragments.FirstFragment;
+import fragments.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnClick;
+    Boolean status = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClick = findViewById(R.id.btnFrg);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                if(status){
+                    FirstFragment firstFragment = new FirstFragment();
+                    fragmentTransaction.add(R.id.fragmentContainer,firstFragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    btnClick.setText("Load Second Fragment");
+                    status = false;
+                }
+                else{
+                    SecondFragment secondFragment = new SecondFragment();
+                    fragmentTransaction.add(R.id.fragmentContainer,secondFragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                    btnClick.setText("Load First Fragment");
+                    status = true;
+                }
+            }
+        });
     }
 }
